07d6dd24a848f66a91f75b7d56107b4f6eb85235
SphericalIce <sphericalice@outlook.com>
2019-09-18 20:05:48 +0100

    Mimic battle records special

--- include/unown_report.h
+++ include/unown_report.h
@@ -1,3 +1,6 @@
 #pragma once
 
 extern u8 gLastResult[];
+void vblank_handler_set(void(*)(void));
+void set_callback2(void(*));
+u32 sub_80CD240();
--- src/unown_report.c
+++ src/unown_report.c
@@ -3,4 +3,8 @@
 
 void UnownReport()
 {
+    vblank_handler_set(0);
+    set_callback2(sub_80CD240);
+    
+    return;
 }
