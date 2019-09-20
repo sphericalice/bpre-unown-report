df1fbfdfd12a8c06423fc99adb0af82cf9bfcd8c
SphericalIce <sphericalice@outlook.com>
2019-09-20 10:38:01 +0100

    Rearrange functions

--- src/unown_report.c
+++ src/unown_report.c
@@ -24,163 +24,38 @@
 #include "graphics/unown.c"
 #include <stddef.h>
 
-static const u8 sTextColors[2][3] =
-{
-    {0, 2, 3},
-    {15, 14, 13}
-};
+static void SetDispcntReg(void);
+static void ClearVramOamPlttRegs(void);
+static void ClearTasksAndGraphicalStructs(void);
+static void ResetBgCoordinates(void);
+static void LoadUnownReportWindowGfx(u8 bgId);
+static void VblankCB_UnownReport(void);
+static void RemoveUnownReportWindow(u8 windowId);
+static void Task_ExitUnownReport(u8 taskId);
+static void Task_BeginPaletteFade(u8 taskId);
+static void Task_UnownReportWaitForPaletteFade(u8 taskId);
+static void MainCB2_UnownReport(void);
+u8 UnownCount();
+static void PrintOnUnownReportWindow(void);
+static u8 GetPage(u8 PageNumber, u8 SwapDirection);
+static void PrintUnownList(u8 PageNumber);
+static void PrintReportPage(u8 PageNumber);
+static void PrintFirstPage();
+static s8 GetPageNumber(u8 taskId, u8 SwapDirection);
+static void SwapPage(u8 taskId, u8 SwapDirection);
+static void Task_CheckUnownReportInput(u8 taskId);
+void CB2_UnownReport();
+void UnownReport();
 
-
-u8 CurrentKindsOfUnownCaught() {
-    // TODO: Actually count the kinds of Unown the player has caught
-    return 28;
-}
-
-static void PrintOnUnownReportWindow(void) {
-    FillWindowPixelRect(0, PIXEL_FILL(0), 0, 0, 256, 0x90);
-    u16 width = 0;
-    StringExpandPlaceholders(gStringVar4, gText_PlayersUnownReport);
-    width = GetStringCenterAlignXOffset(2, gStringVar4, 0xFFFF);
-    AddTextPrinterParameterized3(0, 2, 0x68 - (width >> 1), 40, sTextColors[0], TEXT_SPEED_FF, gStringVar4);
-
-    ConvertIntToDecimalStringN(gStringVar1, CurrentKindsOfUnownCaught(), STR_CONV_MODE_LEFT_ALIGN, 4);
-    StringExpandPlaceholders(gStringVar4, gText_CurrentKinds);
-    width = GetStringCenterAlignXOffset(2, gStringVar4, 0xFFFF);
-    AddTextPrinterParameterized3(0, 2, 0x68 - (width >> 1), 64, sTextColors[0], TEXT_SPEED_FF, gStringVar4);    
-
-    PutWindowTilemap(0);
-    CopyWindowToVram(0, 3);
-}
-
+#define MAX_PAGE_COUNT 9
+#define UNOWN_PER_PAGE 10
 #define PAGE_NEXT 0
 #define PAGE_PREV 1
 #define currentPage data[0]
-#define MAX_PAGE_COUNT 9
-
-static u8 GetPage(u8 PageNumber, u8 SwapDirection) {
-    if (SwapDirection == PAGE_NEXT) {
-        if (PageNumber == MAX_PAGE_COUNT) return PageNumber;
-        if (PageNumber == 0) {
-            if (CurrentKindsOfUnownCaught() == 0) return 4;
-            if (CurrentKindsOfUnownCaught() > 1) return 1;
-        }
-        if (PageNumber == 1) {
-            if (CurrentKindsOfUnownCaught() <= 10) return 4;
-            if (CurrentKindsOfUnownCaught() > 10) return 2;
-        }
-        if (PageNumber == 2) {
-            if (CurrentKindsOfUnownCaught() <= 20) return 4;
-            if (CurrentKindsOfUnownCaught() > 20) return 3;
-        }
-        return PageNumber + 1;
-    }
-    if (SwapDirection == PAGE_PREV) {
-        if (PageNumber == 0) return PageNumber;
-        if (PageNumber == 4) {
-            if (CurrentKindsOfUnownCaught() == 0) return 0;
-            if (CurrentKindsOfUnownCaught() <= 10) return 1;
-            if (CurrentKindsOfUnownCaught() <= 20) return 2;
-            if (CurrentKindsOfUnownCaught() > 20) return 3;
-        }        
-        return PageNumber - 1;
-    }
-    return PageNumber;
-}
-
-static const u8 *UnownStrings[] = {
-    gText_UnownA, gText_UnownB, gText_UnownC, gText_UnownD, gText_UnownE,
-    gText_UnownF, gText_UnownG, gText_UnownH, gText_UnownI, gText_UnownJ,
-    gText_UnownK, gText_UnownL, gText_UnownM, gText_UnownN, gText_UnownO,
-    gText_UnownP, gText_UnownQ, gText_UnownR, gText_UnownS, gText_UnownT,
-    gText_UnownU, gText_UnownV, gText_UnownW, gText_UnownX, gText_UnownY,
-    gText_UnownZ, gText_UnownQuestionMark, gText_UnownExclamationMark
-};
-
-#define UNOWN_PER_PAGE 10
-
-static void PrintUnownList(u8 PageNumber) {
-    u8 row, col, max;
-    max = CurrentKindsOfUnownCaught();
-    if (max > (PageNumber * UNOWN_PER_PAGE)) max = PageNumber * UNOWN_PER_PAGE;
-    if (max > 28) max = 28;
-    for (row = 0, col = 0; row < max-(PageNumber-1) * UNOWN_PER_PAGE; row++, col ^= 1) {
-        AddTextPrinterParameterized3(0,
-                                     2,
-                                     48 + col*88,
-                                     40 + (row - col)*8,
-                                     sTextColors[0],
-                                     TEXT_SPEED_FF,
-                                     UnownStrings[row + (PageNumber-1) * UNOWN_PER_PAGE]);
-    }
-    return;
-}
-
-static const u8 *ReportStrings[] = {
-    gText_Report1, gText_Report2, gText_Report3, gText_Report4, gText_Report5, gText_Report6
-};
-
-static void PrintReportPage(u8 PageNumber) {
-    AddTextPrinterParameterized3(0, 2, 16, 40, sTextColors[0], TEXT_SPEED_FF, ReportStrings[PageNumber]);
-    return;
-}
-
-static void PrintFirstPage() {
-    u16 width = 0;
-    StringExpandPlaceholders(gStringVar4, gText_PlayersUnownReport);
-    width = GetStringCenterAlignXOffset(2, gStringVar4, 0xFFFF);
-    AddTextPrinterParameterized3(0, 2, 0x68 - (width >> 1), 40, sTextColors[0], TEXT_SPEED_FF, gStringVar4);
-
-    ConvertIntToDecimalStringN(gStringVar1, CurrentKindsOfUnownCaught(), STR_CONV_MODE_LEFT_ALIGN, 4);
-    StringExpandPlaceholders(gStringVar4, gText_CurrentKinds);
-    width = GetStringCenterAlignXOffset(2, gStringVar4, 0xFFFF);
-    AddTextPrinterParameterized3(0, 2, 0x68 - (width >> 1), 64, sTextColors[0], TEXT_SPEED_FF, gStringVar4);
-    return;
-}
-
-static s8 GetPageNumber(u8 taskId, u8 SwapDirection) {
-    struct Task *task = &gTasks[taskId];
-    if (GetPage(task->currentPage, SwapDirection) != task->currentPage) {
-        task->currentPage = GetPage(task->currentPage, SwapDirection);            
-        FillWindowPixelBuffer(0, 0);
-        ClearWindowTilemap(0);
-        CopyWindowToVram(0, 3);
-        return task->currentPage;
-    }
-    return -1;
-}
-
-static void SwapPage(u8 taskId, u8 SwapDirection) {
-    s8 PageNumber = GetPageNumber(taskId, SwapDirection);
-    if (PageNumber != -1) {
-        switch (PageNumber) {
-            default:
-            case 0:
-                PrintFirstPage();
-                break;
-            case 1:
-            case 2:
-            case 3:
-                PrintUnownList(PageNumber);
-                break;
-            case 4:
-            case 5:
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-                PrintReportPage(PageNumber-4);
-                break;
-        }
-
-        PlaySE(SE_SELECT);
-        PutWindowTilemap(0);
-        CopyWindowToVram(0, 3);
-    }
-}
 
 u8 *sTilemapBuffer;
+static const u8 sTextColors[2][3] = { {0, 2, 3}, {15, 14, 13} };
 static const struct BgTemplate sUnownReportBgTemplates[] = { { 0, 0, 31, 0, 0, 0, 0 }, { 3, 1, 30, 0, 0, 3, 0 } };
-
 static const struct WindowTemplate sUnownReportWindowTemplates[] =
 {
     {
@@ -193,6 +68,17 @@ static const struct WindowTemplate sUnownReportWindowTemplates[] =
         .baseBlock = 20
     },
     DUMMY_WIN_TEMPLATE
+};
+static const u8 *UnownStrings[] = {
+    gText_UnownA, gText_UnownB, gText_UnownC, gText_UnownD, gText_UnownE,
+    gText_UnownF, gText_UnownG, gText_UnownH, gText_UnownI, gText_UnownJ,
+    gText_UnownK, gText_UnownL, gText_UnownM, gText_UnownN, gText_UnownO,
+    gText_UnownP, gText_UnownQ, gText_UnownR, gText_UnownS, gText_UnownT,
+    gText_UnownU, gText_UnownV, gText_UnownW, gText_UnownX, gText_UnownY,
+    gText_UnownZ, gText_UnownQuestionMark, gText_UnownExclamationMark
+};
+static const u8 *ReportStrings[] = {
+    gText_Report1, gText_Report2, gText_Report3, gText_Report4, gText_Report5, gText_Report6
 };
 
 static void SetDispcntReg(void) {
@@ -280,6 +166,145 @@ static void Task_BeginPaletteFade(u8 taskId) {
     gTasks[taskId].func = Task_ExitUnownReport;
 }
 
+static void Task_UnownReportWaitForPaletteFade(u8 taskId) {
+    if (!gPaletteFade.active)
+        gTasks[taskId].func = Task_CheckUnownReportInput;
+}
+
+static void MainCB2_UnownReport(void) {
+    RunTasks();
+    AnimateSprites();
+    BuildOamBuffer();
+    UpdatePaletteFade();
+}
+
+u8 UnownCount() {
+    // TODO: Actually count the kinds of Unown the player has caught
+    return 28;
+}
+
+static void PrintOnUnownReportWindow(void) {
+    FillWindowPixelRect(0, PIXEL_FILL(0), 0, 0, 256, 0x90);
+    u16 width = 0;
+    StringExpandPlaceholders(gStringVar4, gText_PlayersUnownReport);
+    width = GetStringCenterAlignXOffset(2, gStringVar4, 0xFFFF);
+    AddTextPrinterParameterized3(0, 2, 0x68 - (width >> 1), 40, sTextColors[0], TEXT_SPEED_FF, gStringVar4);
+
+    ConvertIntToDecimalStringN(gStringVar1, UnownCount(), STR_CONV_MODE_LEFT_ALIGN, 4);
+    StringExpandPlaceholders(gStringVar4, gText_CurrentKinds);
+    width = GetStringCenterAlignXOffset(2, gStringVar4, 0xFFFF);
+    AddTextPrinterParameterized3(0, 2, 0x68 - (width >> 1), 64, sTextColors[0], TEXT_SPEED_FF, gStringVar4);    
+
+    PutWindowTilemap(0);
+    CopyWindowToVram(0, 3);
+}
+
+static u8 GetPage(u8 PageNumber, u8 SwapDirection) {
+    if (SwapDirection == PAGE_NEXT) {
+        if (PageNumber == MAX_PAGE_COUNT) return PageNumber;
+        if (PageNumber == 0) {
+            if (UnownCount() == 0) return 4;
+            if (UnownCount() > 1) return 1;
+        }
+        if (PageNumber == 1) {
+            if (UnownCount() <= 10) return 4;
+            if (UnownCount() > 10) return 2;
+        }
+        if (PageNumber == 2) {
+            if (UnownCount() <= 20) return 4;
+            if (UnownCount() > 20) return 3;
+        }
+        return PageNumber + 1;
+    }
+    if (SwapDirection == PAGE_PREV) {
+        if (PageNumber == 0) return PageNumber;
+        if (PageNumber == 4) {
+            if (UnownCount() == 0) return 0;
+            if (UnownCount() <= 10) return 1;
+            if (UnownCount() <= 20) return 2;
+            if (UnownCount() > 20) return 3;
+        }        
+        return PageNumber - 1;
+    }
+    return PageNumber;
+}
+
+static void PrintUnownList(u8 PageNumber) {
+    u8 row, col, max;
+    max = UnownCount();
+    if (max > (PageNumber * UNOWN_PER_PAGE)) max = PageNumber * UNOWN_PER_PAGE;
+    if (max > 28) max = 28;
+    for (row = 0, col = 0; row < max-(PageNumber-1) * UNOWN_PER_PAGE; row++, col ^= 1) {
+        AddTextPrinterParameterized3(0,
+                                     2,
+                                     48 + col*88,
+                                     40 + (row - col)*8,
+                                     sTextColors[0],
+                                     TEXT_SPEED_FF,
+                                     UnownStrings[row + (PageNumber-1) * UNOWN_PER_PAGE]);
+    }
+    return;
+}
+
+static void PrintReportPage(u8 PageNumber) {
+    AddTextPrinterParameterized3(0, 2, 16, 40, sTextColors[0], TEXT_SPEED_FF, ReportStrings[PageNumber]);
+    return;
+}
+
+static void PrintFirstPage() {
+    u16 width = 0;
+    StringExpandPlaceholders(gStringVar4, gText_PlayersUnownReport);
+    width = GetStringCenterAlignXOffset(2, gStringVar4, 0xFFFF);
+    AddTextPrinterParameterized3(0, 2, 0x68 - (width >> 1), 40, sTextColors[0], TEXT_SPEED_FF, gStringVar4);
+
+    ConvertIntToDecimalStringN(gStringVar1, UnownCount(), STR_CONV_MODE_LEFT_ALIGN, 4);
+    StringExpandPlaceholders(gStringVar4, gText_CurrentKinds);
+    width = GetStringCenterAlignXOffset(2, gStringVar4, 0xFFFF);
+    AddTextPrinterParameterized3(0, 2, 0x68 - (width >> 1), 64, sTextColors[0], TEXT_SPEED_FF, gStringVar4);
+    return;
+}
+
+static s8 GetPageNumber(u8 taskId, u8 SwapDirection) {
+    struct Task *task = &gTasks[taskId];
+    if (GetPage(task->currentPage, SwapDirection) != task->currentPage) {
+        task->currentPage = GetPage(task->currentPage, SwapDirection);            
+        FillWindowPixelBuffer(0, 0);
+        ClearWindowTilemap(0);
+        CopyWindowToVram(0, 3);
+        return task->currentPage;
+    }
+    return -1;
+}
+
+static void SwapPage(u8 taskId, u8 SwapDirection) {
+    s8 PageNumber = GetPageNumber(taskId, SwapDirection);
+    if (PageNumber != -1) {
+        switch (PageNumber) {
+            default:
+            case 0:
+                PrintFirstPage();
+                break;
+            case 1:
+            case 2:
+            case 3:
+                PrintUnownList(PageNumber);
+                break;
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                PrintReportPage(PageNumber-4);
+                break;
+        }
+
+        PlaySE(SE_SELECT);
+        PutWindowTilemap(0);
+        CopyWindowToVram(0, 3);
+    }
+}
+
 static void Task_CheckUnownReportInput(u8 taskId) {
     struct Task *task = &gTasks[taskId];
 
@@ -291,18 +316,6 @@ static void Task_CheckUnownReportInput(u8 taskId) {
 
     if (gMain.newKeys & B_BUTTON)
         task->func = Task_BeginPaletteFade;
-}
-
-static void Task_UnownReportWaitForPaletteFade(u8 taskId) {
-    if (!gPaletteFade.active)
-        gTasks[taskId].func = Task_CheckUnownReportInput;
-}
-
-static void MainCB2_UnownReport(void) {
-    RunTasks();
-    AnimateSprites();
-    BuildOamBuffer();
-    UpdatePaletteFade();
 }
 
 void CB2_UnownReport() {
